a9940ebcfb44ae02b7a573973f74178781f606f7
scurry222 <scurry222@gmail.com>
2021-01-14 14:41:33 -0800

    Create Database and generate 100 mortgage entries

--- schema.sql
+++ schema.sql
@@ -5,7 +5,9 @@ CREATE DATABASE mortgage_payments;
 USE mortgage_payments;
 
 CREATE TABLE payments (
-  id NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   home_price INT NOT NULL,
   hoa BIT
-)
+);
+
+INSERT INTO payments (home_price, hoa) VALUES (750000, 0);
