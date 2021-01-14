b3d35d8be2c2102768c932d614cff3d2f5a848fd
scurry222 <scurry222@gmail.com>
2021-01-13 18:37:36 -0800

    Create basic component structure

--- schema.sql
+++ schema.sql
@@ -7,7 +7,5 @@ USE mortgage_payments;
 CREATE TABLE payments (
   id NOT NULL PRIMARY KEY AUTO_INCREMENT,
   home_price INT NOT NULL,
-  down_payment INT NOT NULL,
-  interest_rate INT NOT NULL,
   hoa BIT
 )
