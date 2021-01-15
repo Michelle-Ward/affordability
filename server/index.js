305482d8c54813ffb4ba7bace5ad945cfd1f5075
scurry222 <scurry222@gmail.com>
2021-01-14 16:29:52 -0800

    Add endpoint to grab one home price at id

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { QueryTypes } = require('sequelize');
+const db = require('../database/index');
 
 const PORT = 3000;
 const app = express();
@@ -10,8 +12,13 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/home_price', (req, res) => {
-  res.send();
+app.get('/api/home_price/:id', async (req, res) => {
+  const { id } = req.params;
+  // console.log(id)
+  await db.query(`SELECT * FROM payments WHERE id=${id}`, {
+    type: QueryTypes.SELECT,
+  })
+    .then((pricing) => res.status(200).send(pricing));
 });
 
 app.get('/all_rates', (req, res) => {
