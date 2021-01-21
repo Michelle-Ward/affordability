418a2164126132a1599b37d577af0a1011fdd1ab
scurry222 <scurry222@gmail.com>
2021-01-21 14:40:50 -0800

    Firgure out the numbers

--- client/components/GraphTable.jsx
+++ client/components/GraphTable.jsx
@@ -5,15 +5,16 @@ const GraphTable = ({ state }) => {
   const {
     principle, mortgageIns, tax, insurance,
   } = state;
+  const principlePerMonth = Math.floor(principle / 12);
+  const mortgageInsPerMonth = Math.floor(mortgageIns / 12);
   const section = (data, total) => (data / total) * 100;
-  const total = principle + mortgageIns + tax + insurance;
-
-  const principleSection = section(principle, total);
-  const mortgageInsSection = section(mortgageIns, total);
+  const total = principlePerMonth + mortgageInsPerMonth + tax + insurance;
+  const principleSection = section(principlePerMonth, total);
+  const mortgageInsSection = section(mortgageInsPerMonth, total);
   const taxSection = section(tax, total);
   const insuranceSection = section(insurance, total);
 
-  const taxSectionOffset = 100 - taxSection + 25;
+  const taxSectionOffset = 100 - principleSection + 25;
   const insuranceSectionOffset = taxSectionOffset - taxSection;
   const mortgageInsSectionOffset = insuranceSectionOffset - insuranceSection;
 
@@ -24,21 +25,27 @@ const GraphTable = ({ state }) => {
         cy="21"
         r="15.91549430918952"
         fill="none"
-        stroke="#ceb6ff"
-        strokeWidth="3.8"
-        strokeDasharray={`${mortgageInsSection} ${100 - mortgageInsSection}`}
-        strokeDashoffset={mortgageInsSectionOffset}
-      />
-      <circle
-        cx="21"
-        cy="21"
-        r="15.91549430918952"
-        fill="none"
         stroke="#052286"
         strokeWidth="3.8"
         strokeDasharray={`${principleSection} ${100 - principleSection}`}
         strokeDashoffset="25"
       />
+      {
+        mortgageIns
+          ? (
+            <circle
+              cx="21"
+              cy="21"
+              r="15.91549430918952"
+              fill="none"
+              stroke="#ceb6ff"
+              strokeWidth="3.8"
+              strokeDasharray={`${mortgageInsSection} ${100 - mortgageInsSection}`}
+              strokeDashoffset={mortgageInsSectionOffset}
+            />
+          )
+          : null
+      }
       <circle
         cx="21"
         cy="21"
